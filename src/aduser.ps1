d06a90fc203f8b9400d813452ee959951df35dca
Douam <amadouguedadoba@gmail.com>
2022-05-05 14:39:44 +0200

    fixed commited

--- src/aduser.ps1
+++ src/aduser.ps1
@@ -123,3 +123,55 @@ foreach ($User in $ADUsers) {
 }
 
 Read-Host -Prompt "Press Enter to exit"
+
+
+
+
+
+#AD Deployment 
+
+function Install-Features{
+
+    $FeatureList = @("AD-Domain-Services","DNS")
+
+Foreach($Feature in $FeatureList){
+
+   if(((Get-WindowsFeature-Name $Feature).InstallState)-eq"Available"){
+
+     Write-Output"Feature $Feature will be installed now !"
+
+     Try{
+
+        Add-WindowsFeature-Name $Feature-IncludeManagementTools -IncludeAllSubFeature
+
+        Write-Output"$Feature : Installation is a success !"
+
+     }Catch{
+
+        Write-Output"$Feature : Error during installation !"
+     }
+   } # if(((Get-WindowsFeature -Name $Feature).InstallState) -eq "Available")
+} # Foreach($Feature in $FeatureList)
+}
+
+#Create Activ Directory Domain
+
+$DomainNameDNS = "douam.com"
+$DomainNameNetbios = "DOUAM"
+
+$ForestConfiguration = @{
+'-DatabasePath'= 'C:\Windows\NTDS';
+'-DomainMode' = 'Default';
+'-DomainName' = $DomainNameDNS;
+'-DomainNetbiosName' = $DomainNameNetbios;
+'-ForestMode' = 'Default';
+'-InstallDns' = $true;
+'-LogPath' = 'C:\Windows\NTDS';
+'-NoRebootOnCompletion' = $false;
+'-SysvolPath' = 'C:\Windows\SYSVOL';
+'-Force' = $true;
+'-CreateDnsDelegation' = $false }
+
+Import-Module ADDSDeployment
+Install-ADDSForest @ForestConfiguration
+
