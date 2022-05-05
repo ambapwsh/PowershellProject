45d52d99842735ecf75a1dfbc9ab6fe4ecb723aa
freskimks <78412066+freskimks@users.noreply.github.com>
2022-05-05 17:20:15 +0200

    Update ESGI-GR4-DNS.ps1

--- Freskim/ESGI-GR4-DNS.ps1
+++ Freskim/ESGI-GR4-DNS.ps1
@@ -1,9 +1,7 @@
-#================ Ajouter l'adress ip de l'ad en DNS
+#================ Ajoute de l'adresse IP de l'AD en tant que DNS
 
 $dns1 = "192.168.1.4"
 $dns2 = "168.63.129.16"
 
-
 $interface = Get-NetAdapter | Select-Object  InterfaceIndex
-
 set-DnsClientServerAddress -InterfaceIndex $interface.InterfaceIndex -ServerAddresses ("$dns1","$dns2")
