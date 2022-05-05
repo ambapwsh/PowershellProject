b456f8ad09a6cd8ed6e6ac897976219dfe655e23
freskimks <78412066+freskimks@users.noreply.github.com>
2022-05-05 16:58:07 +0200

    Update ESGI-GR4-Creation-utilisateur-AD.ps1

--- Freskim/ESGI-GR4-Creation-utilisateur-AD.ps1
+++ Freskim/ESGI-GR4-Creation-utilisateur-AD.ps1
@@ -1,104 +1,150 @@
-$foldeBrowser = New-Object System.Windows.Forms.OpenFileDialog
-$foldeBrowser.ShowDialog()
-$file = $foldeBrowser.FileName
-Foreach ($ligne in Get-Content $file)
-{
+#************************************* Variables *************************************
 
-$util = $ligne.Split(":")
-	$pre=$util[0]
-	$nom=$util[1]
-    $gr=$util[2]
-    $acc= $pre
-    $acc= $acc.ToLower()
-	$pathOU = "OU=$ecole,DC=gr4,DC=local"
-    $groupPATH = "OU=$gr,$pathOU"
-    $domain = "gr4.local"
-    $mdp2 = "P@ssw0rd"
-    $mdp = ConvertTo-SecureString $mdp2 -AsPlainText -Force
-    $ecole = "ESGI"
+$location = "CanadaCentral"
+$Groupederessources = "GR4-Groupe-de-ressources"
+$Chrome = "NON" #Oui pour l'installation Google Chrome
+$DNS = "NON" # OUI pour ajouter le DNS 
+$AjDomain = "NON" # OUI pour ajouter la machine dans le domaine
+$domain="gr4.local"
+$PathDNS = "C:\Users\Freskim\Desktop\Project Powershell\DNS.ps1"
+$PathChrome = "C:\Users\Freskim\Desktop\Project Powershell\Google-Chrome.ps1"
+$PathDomain = "C:\Users\Freskim\Desktop\Project Powershell\Domain.ps1"
+
+#************************************* Les Fonctions *************************************
 
 
-$uni=Get-ADOrganizationalUnit -Filter { name -eq $ecole}
-    if ($uni) {Write-Host("L'unite $ecole il existe deja")}
-else{
-	                       #Creation des UNITES 
-New-ADOrganizationalUnit "$ecole"
+#=============================== Creation du login plus mdp local pour les machines ===============================
 
-                           #Message
- Write-Host("L'unite $ecole Il s'est créé avec succès.")
+function CreationMDP {
+$VMLocalUserName = "Freskim"
+$VMLocalAdminSecurePassword = ConvertTo-SecureString "P@ssword" -AsPlainText -Force
+$UserPassword = New-Object System.Management.Automation.PSCredential($VMLocalUserName, $VMLocalAdminSecurePassword)
 }
 
+#La fonction qui permet de demander le nom de la machine virtuelle
+function DemanderNomVM {
+$nomVm = Read-Host "Donner le nom de la machine virtuelle"
+return $nomVm
+}
+#La fonction qui permet de créée la machine virtuelle 
 
+function CreateVM {
+New-AzVm `
+        -ResourceGroupName $Groupederessources `
+        -Name $nomVm `
+        -Location $location `
+        -VirtualNetworkName 'GR4-reseau-virtuel' `
+        -SubnetName 'GR4-sous-reseau' `
+        -SecurityGroupName 'GR4-groupe-de-securite-reseau' `
+        -PublicIpAddressName "GR4-ip-publique-$nomVm" `
+        -Size "Standard_DS1_v2" `
+        -Credential $UserPassword `
+        -OpenPorts 80,3389
 
-
-
-#=======================Création des UNITE====================================
-$uni=Get-ADOrganizationalUnit -Filter { name -eq $gr}
-    if ($uni) {Write-Host("L'unite $gr il existe deja")}
-else{
-	                       #Creation des UNITES 
-New-ADOrganizationalUnit -Name "$gr" -Path $pathOU -ProtectedFromAccidentalDeletion $false
-
-                           #Message
- Write-Host("L'unite $gr Il s'est créé avec succès.")
 }
 
+#La fonction qui permet de Tester si la machine existe  
 
-
-#==============================Création des Groupes============================
-
-
-$grp= get-adgroup -Filter { samaccountname -eq $gr}
-    #if else done
-    if ($grp) {Write-Host("Le Groupe $gr il existe deja")}
-    else{
-    ## Creation du Groupe
-    New-ADGroup -Name "$gr" -SamAccountName $gr -GroupCategory Security -GroupScope Global -Path $groupPATH
-      #Message
-    Write-Host("Le goupe : $gr Il s'est créé avec succès.")
+function TestsiVmExiste {
+$vm = Get-AzVM -ResourceGroupName $Groupederessources
+$vm.Name|foreach{
+    if ($nomVm -eq $_){
+        echo "la machine virtuelle avec le nom $_ existe "
     }
+    else {
+         CreateVM
 
-
-#==============================Création des utilisateurs============================
-
- $usr=Get-ADUser -Filter { SamAccountName -eq $acc}
-    if ($usr) {Write-Host("l'utilisateur $acc il existe deja")}
-
-    else{
-            ##Creation des utilisateurs 
-	New-ADuser -UserPrincipalName $acc@gr4.local -SamAccountName $acc -Name $pre -GivenName $pre -SurName $nom -Path $groupPATH 
- 
-   $date= Get-Date
-            ##mesagge de fin
-    $infoutilisateur += "-------------------------------------$date---------------------------------`nL'utilisateur: $pre $nom`nNom d'utilisateur: $acc`nIl fait partie du groupe : $gr `nle mdp: $mdp2`n"
+         }
+    }
 }
 
 
+#La fonction qui permet d'installer google chrome 
 
-#==============================Création des MDP par defaut============================
+function InstallChrome {
+if ( $Chrome -eq "OUI") {
+Do {
+    $StatusVM = (Get-AzVM -ResourceGroupName $Groupederessources -Name $nomVm -Status).VMAgent.Statuses.DisplayStatus
+    
+    echo "le Status de la vm : $nomVm est $StatusVM "
+    Start-Sleep -s 5
+} until ($StatusVM -eq "Ready")
 
-$usr=Get-ADUser -Filter { SamAccountName -eq $acc}
-    if ($usr) {
-             #Création des MDP par defaut
-                     Set-ADAccountPassword -Identity $acc -NewPassword $mdp –Reset 
-                     Set-ADUser $acc -ChangePasswordAtLogon $true
-	                 Write-Host("L'utilisateur $acc à pour mdp : $mdp2")}
-else {Write-Host("l'utilisateur $acc il n'existe pas")
+if ($StatusVM -eq "Ready"){
+Invoke-AzVmRunCommand `
+     -ResourceGroupName $Groupederessources `
+     -VMName "$nomVm" `
+     -CommandId "RunPowerShellScript" `
+     -ScriptPath $PathChrome
+     }
+}
 }
 
 
+#La fonction qui permet de configurer le DNS sur les machines
 
-#==============================Activer les utilisateurs============================
-
-$usr=Get-ADUser -Filter { SamAccountName -eq $acc}
-    if ($usr) {
-       #Activer les utilisateurs
-   Set-ADUser -Identity $acc -Enabled $true
-   Write-Host("L'utilisateur $acc il est Activer")
-   }
-   else {Write-Host("l'utilisateur $acc il n'existe pas")}
-
+function AjouterDNS{
+if ( $DNS -eq "OUI") {
+Do {
+    $StatusVM = (Get-AzVM -ResourceGroupName $Groupederessources -Name $nomVm -Status).VMAgent.Statuses.DisplayStatus
+    echo "En attente de la finalisation de la Machine Virtuelle pour configurer le DNS"
+    sleep 5
+} until ($StatusVM -eq "Ready")
 
 
+if ($StatusVM -eq "Ready"){
+Invoke-AzVmRunCommand `
+     -ResourceGroupName $Groupederessources `
+     -VMName "$nomVm" `
+     -CommandId "RunPowerShellScript" `
+     -ScriptPath $PathDNS
+}
 
 }
+}
+
+#La fonction qui permet d'Ajouter la VM dans le Domaine
+
+function AjouterlaVmdansleDomain {
+if ($AjDomain -eq "OUI"){
+Do {
+    $StatusVM = (Get-AzVM -ResourceGroupName $Groupederessources -Name $nomVm -Status).VMAgent.Statuses.DisplayStatus
+    echo "En attente de la finalisation de la Machine Virtuelle pour ajouter la machine sur le Domaine"
+    sleep 5
+} until ($StatusVM -eq "Ready")
+
+if ($StatusVM -eq "Ready"){
+
+Invoke-AzVmRunCommand `
+     -ResourceGroupName $Groupederessources `
+     -VMName "$nomVm" `
+     -CommandId "RunPowerShellScript" `
+     -ScriptPath $PathDomain
+     }
+}
+
+
+if ($AjDomain -eq "OUI"){
+if($?){
+    Restart-AzVM -Name $nomVm -ResourceGroupName $Groupederessources
+    echo " la machine $nomVm vien d'être redemarré"
+}
+else{
+    echo "l'integration de la machine dans l'AD n'a pas fonctionnée"
+}
+}
+
+}
+
+#=============================== création du mdp
+CreationMDP
+#=============================== Demander à l'utilisateur le nom de la VM
+DemanderNomVM
+#=============================== Tester si la machine existe 
+TestsiVmExiste
+#===============================  Installation de Google Chrome
+InstallChrome
+#================ Ajoute de l'adresse ip de l'AD en tant que DNS
+AjouterDNS
+#================ Ajout de la Machine Virtuelle dans le domaine
+AjouterlaVmdansleDomain
